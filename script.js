ceed753cb7ebc1cb341d27481b82d5eed8c8bbe9
xjeffss <elmorejeffreyp@johndeere.com>
2021-01-08 16:00:01 -0600

    Project-1-Hangman-letter some java

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log("hooked up")
 
-const winnerBlue = document.querySelector(".winner");
+const instructions = document.querySelector(".instructions");
 // winnerBlue.innerHTML = "Player 2 is the winner";
-console.log(winnerBlue.innerText)
+console.log(instructions.innerText);
+const boxClick = document.querySelectorAll(".box");
+boxClick.addEventListener("click", clicker);
+function clicker(){
+    console.log(boxClick)
+}
+
