8f5a50677a0e8d7834e42a5f115b61944bd94b18
xjeffss <elmorejeffreyp@johndeere.com>
2021-01-09 21:23:28 -0600

    Project-1-Hangman-letter letter buttons work

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ console.log("hooked up")
 const instructions = document.querySelector(".instructions");
 // winnerBlue.innerHTML = "Player 2 is the winner";
 console.log(instructions.innerText);
-const boxClick = document.querySelectorAll(".box");
-boxClick.addEventListener("click", clicker);
-function clicker(){
-    console.log(boxClick)
+
+const boxClick = document.getElementsByClassName("box");
+
+for (let i=0; i<boxClick.length; i++) {
+   boxClick[i].addEventListener("click", clicker); 
+
 }
 
+function clicker(event){
+    event.target.style.backgroundColor = "gray";
+    event.target.removeEventListener("click", clicker);
+    console.log(event.target.innerText)
+}
